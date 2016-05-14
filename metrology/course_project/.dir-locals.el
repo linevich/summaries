1d3db050c91769b2371ad446bfc21a41bc8cc348
linevich <linevich@swebion.com>
2016-05-14 22:54:57 +0300

    Додано налаштування Emacs для курсового

---
+++ metrology/course_project/.dir-locals.el
@@ -0,0 +1,20 @@
+((nil . ((eval . (progn
+		   (defun make-summary-pdf ()
+		     "Compiles pdf from current summary."
+		     (interactive)
+		     (message "Building PDF")
+		     (async-shell-command
+		      (concat "cd "
+			      (file-name-directory buffer-file-name)
+			      " && make pdf && mupdf build/document.pdf") nil nil))
+
+		   (defvar summary-mode-map (make-sparse-keymap))
+		   (define-minor-mode summary-mode
+		     "Minor mode to add keybindngs to my summaries folder"
+		     :init-value nil
+		     :ligter: summary
+		     summary-mode-map
+		     )
+		   
+		   (define-key summary-mode-map (kbd "C-b") 'make-summary-pdf)
+		   (summary-mode t))))))
