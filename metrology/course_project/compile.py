ea7813adffe5dde8148ee47e144d1f27c0653fea
linevich <linevich@swebion.com>
2016-06-21 03:47:13 +0300

    Збірка PDF, розділ 3

--- metrology/course_project/compile.py
+++ metrology/course_project/compile.py
@@ -10,4 +10,5 @@ print_to_file(print_observations_confident_interval)
 print_to_file(print_sensitivity)
 print_to_file(print_sensitivity_am)
 print_to_file(print_sensitivity_table)
+print_to_file(print_delta_sum)
 
